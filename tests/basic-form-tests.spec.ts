1293e7a34093310471ca2b341a60b7212f877f33
Barbara Taskinen <barbara.taskinen@gmail.com>
2026-02-23 11:15:44 +0200

    Create test for successful form submission

---
+++ tests/basic-form-tests.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test'
+
+const firstName = 'Jane';
+const lastName = 'Doe';
+const email = 'jane.doe@email.com';
+
+test.describe('Basic Form tests', () => {
+  test.beforeEach('Navigate to url', async ({ page}) => {
+    await page.goto('https://formio.github.io/angular-demo/#/');
+    await expect(page.getByText('Example Form')).toBeVisible();
+  })
+
+  test('Successful submission with all required fields filled', async({ page }) => {
+    const firstNameInput = page.getByLabel('First Name');
+    const lastNameInput = page.getByLabel('Last Name');
+    const emailInput = page.getByLabel('Email');
+    const submitButton = page.getByRole('button', { name: 'Submit'});
+
+    await expect(firstNameInput).toBeVisible();
+    await firstNameInput.fill(firstName);
+
+    await expect(lastNameInput).toBeVisible();
+    await lastNameInput.fill(lastName);
+
+    await expect(emailInput).toBeVisible();
+    await emailInput.fill(email);
+
+    await expect(submitButton).toBeVisible();
+    await submitButton.click();
+    await expect(page.getByRole('alert')).toBeVisible();
+    await expect(page.getByRole('alert')).toHaveText('Submission Complete.');
+    await expect(page.locator('.help-block')).toBeVisible();
+    await expect(page.locator('.help-block')).toHaveText('Submission Complete');
+
+  })
+})
